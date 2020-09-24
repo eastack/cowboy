7bf429e92468fd837702a251d118338b250839fa
radix10 <admin@radix10.me>
2020-09-24 17:37:51 +0800

    print scope

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ try {
   
     var putExtra = new qiniu.form_up.PutExtra();
 
-    core.info(`Options is ${options}`)
+    core.info(`Scope is ${options.scope}`)
     core.info(`Download ${url} to ${bucket}/${key}...`)
     formUploader.putStream(uploadToken, key, response, putExtra, (error, body, info) => {
       if (error) {
