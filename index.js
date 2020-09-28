2f5502eee578499a5362ed008b74374bcc8128de
radix10 <admin@radix10.me>
2020-09-28 18:21:34 +0800

    reimplement

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const core = require('@actions/core');
 const qiniu = require('qiniu')
-const {https} = require('follow-redirects');
+const request = require('request')
+const fs = require('fs')
 
+const path = '/tmp/cowboy'
 const url = core.getInput('url');
 const accessKey = core.getInput('access_key');
 const secretKey = core.getInput('secret_key');
@@ -10,31 +12,30 @@ const key = core.getInput('key');
 const overwrite = core.getInput('overwrite');
 
 try {
-  https.get(url, response => {
-    var mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
-    var options = overwrite.toLowerCase() === 'yes' ? {scope: `${bucket}:${key}`} : {scope: bucket};
-    var putPolicy = new qiniu.rs.PutPolicy(options);
-    var uploadToken = putPolicy.uploadToken(mac);
-  
-    var config = new qiniu.conf.Config();
-    var formUploader = new qiniu.form_up.FormUploader(config);
-  
-    var putExtra = new qiniu.form_up.PutExtra();
+    const mac = new qiniu.auth.digest.Mac(accessKey, secretKey);
+    const options = overwrite.toLowerCase() === 'yes' ? {scope: `${bucket}:${key}`} : {scope: bucket};
+    const putPolicy = new qiniu.rs.PutPolicy(options);
+    const uploadToken = putPolicy.uploadToken(mac);
+    const config = new qiniu.conf.Config();
+    const formUploader = new qiniu.form_up.FormUploader(config);
+    const putExtra = new qiniu.form_up.PutExtra();
 
-    core.info(`Download ${url} to ${bucket}:${key}`)
-    formUploader.putStream(uploadToken, key, response, putExtra, (error, body, info) => {
-      if (error) {
-        throw error;
-      }
-      if (info.statusCode === 200) {
-          core.setOutput("hash", body.hash);
-          core.setOutput("key", body.key);
-      } else {
-        console.log(info.statusCode);
-        console.log(body);
-      }
+    request(url).pipe(fs.createWriteStream(path))
+        .on('close', () => core.info(`Download ${url} to ${path} done.`))
+
+    formUploader.putFile(uploadToken, key, path, putExtra, (error, body, info) => {
+        if (error) {
+            throw error;
+        }
+        if (info.statusCode === 200) {
+            core.setOutput("hash", body.hash);
+            core.setOutput("key", body.key);
+        } else {
+            core.warning(info.statusCode);
+            core.warning(body);
+        }
+
     });
-  })
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
